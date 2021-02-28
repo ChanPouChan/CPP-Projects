d836d60c52d726b105b786755f7ac3cff234a61b
Giorgos Georgakis <61576875+ChanPouChan@users.noreply.github.com>
2021-02-28 23:16:28 +0200

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	char name[20];
+	char surname[20];
+	int age;
+	
+	cout<<"Write name:\n";
+	cin>>name;
+	cout<<"Write surname:\n";
+	cin>>surname;
+	cout<<"Write age:\n";
+	cin>>age;
+	cout<<name<<" "<<surname<<" "<<age<<endl;
+	
+	return 0;
+}
